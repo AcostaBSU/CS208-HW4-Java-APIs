d5f88ea09df71a8dc48dcf539bcdc3a7bce8cae5
AcostaBSU <acostabsu@gmail.com>
2024-02-06 18:08:12 -0700

    Implement GET route for first & last name

--- src/main/java/cs208/PracticeController.java
+++ src/main/java/cs208/PracticeController.java
@@ -1,12 +1,31 @@
 package cs208;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PracticeController
 {
     // TODO: create a GET route with two query parameters
+    // GET /
+    @GetMapping("/display_name")
+    String getNameFromQueryString(@RequestParam("first_name") String firstName,
+                                     @RequestParam("last_name") String lastName)
+    {
+        System.out.println("Start of assigning parameters:");
+        System.out.println("First Name : " + firstName);
+        System.out.println("Last Name  : " + lastName);
+        System.out.println("End of assigning parameters:");
 
+        String valueReturnedToBrowser =
+                "These are the values for the parameters passed in the Browser URL:<br>" +
+                "First Name : " + firstName + "<br>" +
+                "Last Name  : " + lastName + "<br>" +
+                "See getNameFromQueryString() to make changes to the webpage output seen here.";
+
+        return valueReturnedToBrowser;
+    }
 
     // TODO: create a GET route with a path parameter
 
